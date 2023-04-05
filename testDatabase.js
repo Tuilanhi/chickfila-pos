2ecd658eb99ac8c1987b6a278ffaae3f1e4d7620
Nhi Vu <vuthuynhi05@gmail.com>
2023-04-05 10:36:55 -0500

    added database and query function

---
+++ testDatabase.js
@@ -0,0 +1,26 @@
+const Database = require("./Database");
+
+class testDatabase {
+  constructor() {
+    this.db = new Database();
+  }
+
+  async getFirstFiveItems(command) {
+    await this.db.connect();
+
+    const sql = command;
+    const items = await this.db.query(sql);
+
+    await this.db.disconnect();
+
+    return items;
+  }
+}
+
+async function main() {
+  const menu = new testDatabase();
+  const items = await menu.getFirstFiveItems("SELECT * FROM menu LIMIT 5;");
+  console.log(items);
+}
+
+main();
