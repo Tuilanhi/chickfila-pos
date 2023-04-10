5dbb2e3534f4e13d215591f1a6b527c24de0711d
Pablo Pineda <pablopineda@tamu.edu>
2023-04-09 20:17:35 -0500

    Menu has been transcribed but not tested

--- Menu.js
+++ Menu.js
@@ -1,22 +1,16 @@
 import { Database } from "./Database.js";
 
 class Menu {
-    constructor(Item, price=0, category=0) {
-      this.db = new Database();
-      this.Item = Item;
-      this.price = price;
-      this.category = category;
-      if(price == 0 & category == 0){
-        this.removeItem(Item);
-      }
-      else {
-        this.setItem(Item, price, category);
-      }
+    constructor(Item, price, category) {
+      this.setItem(Item, price, category);
+    }
+  
+    constructor(Item) {
+      this.removeItem(Item);
     }
   
     async setItem(Item, price, category) {
       try {
-        this.db.connect();
         // if item name doesn't exist add new Item
         // get names of existing items
         let sqlStatement = `SELECT COUNT(*) FROM menu WHERE item ='${Item}'`;
@@ -40,7 +34,6 @@ class Menu {
       } catch (e) {
         console.error(e);
       } 
-      await this.db.disconnect();
     }
   
     async removeItem(Item) {
