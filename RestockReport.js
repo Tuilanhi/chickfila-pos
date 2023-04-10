921de171e676f5d92c2687c27a328a592c6f8f36
raqueloseguera <112505790+raqueloseguera@users.noreply.github.com>
2023-04-09 19:03:56 -0500

    Add files via upload

--- RestockReport.js
+++ RestockReport.js
@@ -1,11 +1,14 @@
-const { Client } = require('pg');
+
+const Database = require("./Database");
 
 /**
  * It takes in an Array of Strings and calls the parseCart method.
  */
 class RestockReport {
   constructor() {
+    this.db = new Database();
     this.restock();
+    console.log('Opened database');
   }
 
   /**
@@ -13,39 +16,26 @@ class RestockReport {
    *
    * @return A client object to the database.
    */
-  static connection() {
-    // Building the connection with your credentials
-    const teamNumber = 'team_1';
-    const dbName = `csce315331_${teamNumber}`;
-    const dbConnectionString = `postgresql://csce315331_team_1_master:TEAM_1@csce-315-db.engr.tamu.edu/${dbName}`;
 
-    // Connecting to the database
-    const client = new Client({
-      connectionString: dbConnectionString,
-    });
-    client.connect();
-    console.log('Opened database successfully');
-
-    return client;
-  }
 
   async restock() {
     let result = null;
     try {
       //Inserting new Item into Bridge in database
-      const client = RestockReport.connection();
+      this.db.connect();
+      console.log('Opened database successfully');
       const minimum = 150;
       const sqlStatement = `SELECT * FROM ingredients WHERE quantity <= ${minimum};`;
-      result = await client.query(sqlStatement);
+      result = await this.db.query(sqlStatement);
 
-      result.rows.forEach((row) => {
+      result.forEach((row) => {
         console.log(`${row.ingredient} ${row.quantity} ${row.unit} ${row.type}`);
       });
-      client.end();
     } catch (e) {
       console.error(e);
       process.exit(0);
     }
+    await this.db.disconnect();
     return result;
   }
 }
