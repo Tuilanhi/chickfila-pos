624e64e537cfe39961a8f9bc6f0858d7abc17b43
raqueloseguera <112505790+raqueloseguera@users.noreply.github.com>
2023-04-09 19:24:02 -0500

    Add files via upload

--- NewMenuItem.js
+++ NewMenuItem.js
@@ -1,43 +1,18 @@
-const { Client } = require('pg');
+const Database = require("./Database");
 
 /**
  * It takes in an array of strings and calls the addNewItem method
  */
 class NewMenuItem {
   constructor(newItem, newIngredients) {
+    this.db = new Database();
     this.newItem = newItem;
     this.newIngredients = newIngredients;
     this.newIngredientsArray = [];
     this.addNewItem(newItem, newIngredients);
   }
 
-  /*
-   * It connects to the database and returns a client object
-   *
-   * @return A client object to the database.
-   */
-  async connect() {
-    // Building the connection with your credentials
-    const client = new Client({
-      user: 'csce315331_team_1_master',
-      host: 'csce-315-db.engr.tamu.edu',
-      database: 'csce315331_team_1',
-      password: 'TEAM_1',
-      port: 5432,
-    });
-
-    // Connecting to the database
-    try {
-      await client.connect();
-    } catch (e) {
-      console.error('Error connecting to the database:', e);
-      throw e;
-    }
-
-    console.log('Database connection successful');
-
-    return client;
-  }
+  
 
   async addNewItem(itemName, ingredients) {
     const newIngredientsArray = ingredients.split(',');
@@ -54,10 +29,12 @@ class NewMenuItem {
     }
 
     try {
-      const client = await this.connect();
+      this.db.connect();
+      console.log('Opened database successfully');
+
       // Inserting new Item into Bridge in database
       const sqlStatement = `INSERT INTO Bridge (item, ingredients) VALUES ('${itemName}', '{${ingredients}}')`;
-      const res = await client.query(sqlStatement);
+      const res = await this.db.query(sqlStatement);
       console.log('Added New Item to Bridge');
       console.log(ingredients);
 
@@ -69,20 +46,22 @@ class NewMenuItem {
         const sqlStatement = `SELECT COUNT(*) FROM ingredients WHERE ingredient ='${Ingredient}'`;
 
         // call methods that might throw SQLException
-        const result_1 = await client.query(sqlStatement);
-        const count = result_1.rows[0].count;
+        const result_1 = await this.db.query(sqlStatement);
+        const count = result_1[0].count;
         // add item if it doesn't exists
         if (count === 0) {
           const sqlStatement = `INSERT INTO ingredients (Ingredient, Quantity, Unit, Type) VALUES ('${Ingredient}', 500, '${Unit}', '${Type}')`;
-          const result_2 = await client.query(sqlStatement);
+          const result_2 = await this.db.query(sqlStatement);
           console.log('added');
         }
-      }
-      await client.end();
+    }
+
+
     } catch (e) {
       console.error('Error while adding new item:', e);
       throw e;
     }
+    await this.db.disconnect();
   }
 }
 
