0b449f08fe67f193ec0991e28f1a5e091b0247d5
Nhi Vu <vuthuynhi05@gmail.com>
2023-04-24 10:30:41 -0500

    changed callback url for testing

--- LoginPage.js
+++ LoginPage.js
@@ -89,7 +89,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: "https://csce315-web-app.onrender.com/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       userProfile = profile;
