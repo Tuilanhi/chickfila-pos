a966bf5f9c3f5a257645a53e61be5e1402f7dc4b
Pablo Pineda <pablopineda@tamu.edu>
2023-05-01 13:41:22 -0500

    Z Report finished

--- Z-Report.js
+++ Z-Report.js
@@ -1,17 +1,18 @@
 import { Database } from "./Database.js";
 
+
 class ZReport{
     constructor(){
         this.db = new Database();
     }
-    async zrep(date1, date2){
+    async zrep(){
         try {
             await this.db.connect();
             // if item name doesn't exist add new Item
             // get names of existing items
-            let sqlStatement = "SELECT days, SUM(totalsales) as totalsales FROM chickfila_sales GROUP BY days";
+            let sqlStatement = "SELECT days, SUM(totalsales) as totalsales FROM chickfila_sales GROUP BY days Order BY days";
             let result = await this.db.query(sqlStatement);
-            const count = result[0]["count"];
+            console.log(result);
             
       
             await this.db.disconnect();
@@ -20,5 +21,7 @@ class ZReport{
           }
     }
 }
+
+const run = new ZReport().zrep();
 
 export { ZReport };
